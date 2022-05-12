c46d9f8c95f7b85ad364eb2416b99b68188f2a16
JakubKrol <contact@itdotfocus.com>
2022-05-12 17:55:35 +0200

    Added Context for search value.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from "./components/layout/Header";
 import {Map} from "./components/Map/Map";
+import {SearchContext} from './contexts/search.context';
 
 export const App = () => {
+    const [search, setSearch] = useState('');
+
     return (
-        <>
+        <SearchContext.Provider value={{search, setSearch}}>
             <Header/>
             <Map/>
-        </>
+        </SearchContext.Provider>
     );
 };
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icon';
+import {SearchContext} from "../../contexts/search.context";
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+
+    useEffect(() => {
+        console.log('Make request to search for', search);
+    }, [search]);
+
     return (
         <div className="map">
+            <h1>Search for: {search}</h1>
             <MapContainer center={[50.2657152,18.9945008]} zoom={20}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
